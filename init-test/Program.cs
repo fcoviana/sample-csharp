b6d6e8388c3fcc73d1461cac696106e1ceee0bbd
Francisco Viana <franciscoviana862@gmail.com>
2023-03-11 10:55:00 -0300

    init obj

---
+++ init-test/Program.cs
@@ -0,0 +1,11 @@
+﻿class TestClass
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Olá mundo!");
+    var user = new User();
+    user.name = "Francisco";
+    user.age = 21;
+    Console.WriteLine(user.ToString());
+  }
+}
---
+++ init-test/User.cs
@@ -0,0 +1,10 @@
+﻿public class User
+{
+  public string? name { get; set; }
+  public int age { get; set; }
+
+  public override string ToString()
+  {
+    return base.ToString() + ": { name: " + this.name + "; age: " + this.age + " }";
+  }
+}
